d58421fa2e2428bee936850dc358a97c2b7973a5
Smriti Grover <104589620+smritig26@users.noreply.github.com>
2024-10-07 21:52:50 +0530

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,6 +1,6 @@
 import random
 
-# Print multiline instruction
+
 print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
       + "Rock vs Paper -> Paper wins \n"
       + "Rock vs Scissors -> Rock wins \n"
@@ -10,14 +10,13 @@ while True:
 
     print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
 
-    # Take the input from user
+
     choice = int(input("Enter your choice: "))
 
-    # Looping until user enters valid input
+   
     while choice > 3 or choice < 1:
         choice = int(input('Enter a valid choice please : '))
 
-    # Initialize value of choice_name variable corresponding to the choice value
     if choice == 1:
         choice_name = 'Rock'
     elif choice == 2:
@@ -25,14 +24,12 @@ while True:
     else:
         choice_name = 'Scissors'
 
-    # Print user choice
+    
     print('User choice is:', choice_name)
     print("Now it's Computer's Turn...")
 
-    # Computer chooses randomly any number among 1, 2, and 3
     comp_choice = random.randint(1, 3)
 
-    # Initialize value of comp_choice_name variable corresponding to the choice value
     if comp_choice == 1:
         comp_choice_name = 'Rock'
     elif comp_choice == 2:
@@ -43,7 +40,6 @@ while True:
     print("Computer choice is:", comp_choice_name)
     print(choice_name, 'vs', comp_choice_name)
 
-    # Determine the winner
     if choice == comp_choice:
         result = "DRAW"
     elif (choice == 1 and comp_choice == 2) or (comp_choice == 1 and choice == 2):
@@ -53,7 +49,6 @@ while True:
     elif (choice == 2 and comp_choice == 3) or (comp_choice == 2 and choice == 3):
         result = 'Scissors'
 
-    # Print the result
     if result == "DRAW":
         print("<== It's a tie! ==>")
     elif result == choice_name:
@@ -61,7 +56,6 @@ while True:
     else:
         print("<== Computer wins! ==>")
 
-    # Ask if the user wants to play again
     print("Do you want to play again? (Y/N)")
     ans = input().lower()
     if ans == 'n':
